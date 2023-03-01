7054c0003851e703f3c980ae0f5e2b086002a1e2
Victoria Gullotti <victoriagullotti101@gmail.com>
2023-03-01 10:53:57 +0800

    Initial changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import pprint
+import pprint   #   Prety Print
 
+#   Connecting to the News Hacker website
 res = requests.get('https://news.ycombinator.com/news')
 res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
 soup2 = BeautifulSoup(res2.text, 'html.parser')
 
-links = soup.select('.titleline > a')  # heads up! .storylink changed to .titleline
+links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
-links2 = soup2.select('.titleline > a')  # heads up! .storylink changed to .titleline
+links2 = soup2.select('.titleline > a')
 subtext2 = soup2.select('.subtext')
 
 mega_links = links + links2
@@ -28,6 +29,8 @@ def create_custom_hn(links, subtext):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
+
+            #   Only 100 points and more stories
             if points > 99:
                 hn.append({'title': title, 'link': href, 'votes': points})
     return sort_stories_by_votes(hn)
